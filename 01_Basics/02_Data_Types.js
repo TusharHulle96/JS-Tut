1615e8accc0a6064aa25e87f44351aef8a0f3a7f
TusharHulle96 <98300975+TusharHulle96@users.noreply.github.com>
2024-08-17 05:10:50 +0000

    data_types

---
+++ 01_Basics/02_Data_Types.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all Js Code as newer version 
+
+// alert(3+3) // we are using  nodejs not browser 
+console.log(3+3); 
+console.log("Tushar") 
+
+let name= "Tushar" 
+let age = 18 
+let isloggedIn = false 
+let state;
+
+// number => to power 53 
+// bigInt 
+// string => "" 
+// boolean = true /false 
+// null => standalone value 
+// undefined => 
+// symbol  => unique  
+
+// object 
+
+console.log(typeof null) // object 
+console.log(typeof undefined) //undefined 
---
+++ 01_Basics/Variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accontCity = "Jaipur" 
+
+// accountId = 2
+
+
+accountEmail ="hc@hc.com" 
+accountPassword ="212121212"
+accountCity = "Bengluru"
+let accountstate;
+console.log(accountId); 
+
+console.table([accountId, accountEmail,accountPassword,accontCity,accountstate])   
+
