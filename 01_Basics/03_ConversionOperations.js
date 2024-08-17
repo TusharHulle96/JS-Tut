3f7651339f7deee1846062bd162e54394070ebf5
TusharHulle96 <98300975+TusharHulle96@users.noreply.github.com>
2024-08-17 06:01:38 +0000

    COnversionof Datatypes

---
+++ 01_Basics/03_ConversionOperations.js
@@ -0,0 +1,21 @@
+let score= "TUshar"
+
+// console.log(typeof score)
+// console.log(typeof (score)) 
+
+let valueInNumber = Number(score) 
+// console.log( valueInNumber) 
+
+
+// "33"=> 33 
+// "33abc" => NaN 
+// true => 1; false=> 0 
+
+let isLoggedIn = 1 
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+let somenumber = 33 
+
+let stringNumber = String(somenumber) 
+console.log(typeof  stringNumber); 
