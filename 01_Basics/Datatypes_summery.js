e95e5bc3d736ef9d92ea8f21ccdd2cae25049349
TusharHulle96 <98300975+TusharHulle96@users.noreply.github.com>
2024-08-19 05:55:00 +0000

    Data_Type SUmmery

---
+++ 01_Basics/Datatypes_summery.js
@@ -0,0 +1,39 @@
+// Primitive Data types 
+
+// 7 types :String , Number, Boolean , null ,undefined, Symbol ,bigInt , 
+
+const score =100
+const scoreValue = 100.3
+
+const isLoggedIn = false 
+const outsideTemp = null 
+let userEmail;
+
+const id = Symbol('123') 
+const anotherId= Symbol('123') 
+
+// console.log(id === anotherId)
+
+const bigNumber = 3456234567890n
+
+
+
+// Reference (Non primitive) 
+ 
+// Array , Objects , Function     
+
+const heros = ["Shaktiman","nagraj",'Doga'];
+let myObj = {
+    name:"Tushar",
+    age:22,
+}
+
+
+const myFunction = function(){
+    console.log("Hello World");
+
+}
+
+console.log(typeof anotherId);  
+
+
